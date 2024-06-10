516d0826e383d55897672f07a7e5f00ca19bea8e
Corner <wall.corner@outlook.com>
2024-06-10 22:52:10 +0800

    feat: zhaozy test

--- src/test/java/ZhaozyTest.java
+++ src/test/java/ZhaozyTest.java
@@ -2,6 +2,8 @@ import com.github.catvod.spider.Zhaozy;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class ZhaozyTest {
     private static Zhaozy instance;
 
@@ -14,6 +16,13 @@ public class ZhaozyTest {
     @Test
     public void searchTest() throws Exception {
         String s = instance.searchContent("阿凡达", false);
+        System.out.println(s);
+        AssertUtil.INSTANCE.assertResult(s);
+    }
+
+    @Test
+    public void detailTest() throws Exception {
+        String s = instance.detailContent(Arrays.asList("g8KdIBFD0BA1t"));
         System.out.println(s);
         AssertUtil.INSTANCE.assertResult(s);
     }
